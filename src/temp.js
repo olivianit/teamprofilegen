6d41e6552036fdbc3322dcae23326e1e4fc38e92
Aivi <olivianitzel@gmail.com>
2023-06-07 15:48:57 -0400

    card #2

--- src/temp.js
+++ src/temp.js
@@ -15,3 +15,21 @@ const managerCard = (manager) => {
     </div>
     `;
 };
+
+const engineerCard = (engineer) => {
+    return `
+    <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${engineer.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${engineer.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class="list-group-item">GitHub: <a href="${engineer.github}" target="_blank" rel="noopener noreferrer">${engineer.github}</a></li>
+            </ul>
+        </div>
+    </div>
+    `;
+};
